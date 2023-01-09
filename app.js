002371ddba1885d05cc65f2cc2044e1883239121
Elie Ruvinga <ruvingaelie@gmail.com>
2023-01-09 03:36:21 +0100

    initial codebase

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// notre Application EPRESS
+
+const express = require('express');
+const app = express();  // methode express
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methodes', 'GET', 'POST');
+    next();
+})
+app.use('/part-one/all-stuff', (req, res) => {
+
+    const stuff = [
+        {
+            _id: '1234',
+            title: "mon article",
+            description: "description de l'article",
+            imageUrl: "",
+            useId: 25
+        }
+    ]
+    res.json(stuff);
+    res.status(200);
+});
+
+
+
+module.exports = app;
