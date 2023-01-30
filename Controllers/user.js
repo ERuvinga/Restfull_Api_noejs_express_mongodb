93771e11e91f1fc3c172e89f9d85050fa7454d23
Elie Ruvinga <ruvingaelie@gmail.com>
2023-01-30 17:32:45 +0100

    adding a singup function white bcrypt package of hashing a password

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,10 +1,36 @@
 
 const user = require('../Models/users');
+const bcrypt = require('bcrypt'); 
 
 exports.login = (req, res) =>{
-    console.log(req.body);
+    console.log("login data");
 };
 
 exports.signup = (req, res) =>{
-    console.log(req.body);
+    const password = req.body.password;
+    const saltpassword = 10;
+    
+    bcrypt.hash(password, saltpassword) //hashing a password
+    .then(hash => { // if operation is okey, create un new user
+        const User = new user({
+           ...req.body,
+        })
+
+        User.save() // save a new user in database from 'user' model
+        .then( ()=> {
+            res.status(200); // response a client
+            res.json({message: 'new user Created'})
+        })
+        .catch(error => {  // if have Exception whene you save a data in database
+            console.log(`MessageError -> ${error}`);
+            res.status(404);
+            res.json({ MessageError : error})
+        })
+    })
+    .catch( error => {
+        console.log(error);
+        res.status(500);
+        res.json(error);
+    })
+
 };
--- Models/users.js
+++ Models/users.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const userModel = mongoose.model("user", 
     {
-        email:{type: String, require: true},
-        password: {type: Number, require: true}
+        email:{type: String, require: true, unique: true},
+        password: {type: String, require: true}
     });
 
 module.exports = userModel;
